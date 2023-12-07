1c2f358f0fc2fc4f31320dd7053bbe31465ac26c
Shivaprasadm02 <64781205+Shivaprasadm02@users.noreply.github.com>
2023-12-07 15:14:17 +0530

    Add files via upload

---
+++ assignments/a4/2.sv
@@ -0,0 +1,23 @@
+module top;
+class parent;
+rand bit[2:0]a;
+constraint a_size{a==3;}
+endclass
+class child extends parent;
+rand bit[2:0]a;
+constraint b_size{a==2;}
+constraint c_size{super.a==a;}
+endclass
+
+parent p;
+child c;
+initial
+begin
+c=new;
+repeat(7)
+begin
+c.randomize;
+$display("child: %p", c);
+end
+end
+endmodule
---
+++ assignments/a4/5.sv
@@ -0,0 +1,21 @@
+module top;
+class parent;
+	rand bit[2:0]a;
+	constraint a_size{a<3;}
+endclass
+
+class child extends parent;
+	rand bit[2:0]a;
+	constraint b_size{a<5;}
+endclass
+
+parent p;
+child c=new;
+
+initial
+	repeat(7)
+	begin
+	c.randomize;
+	$display("child: %p", c);
+	end
+endmodule
